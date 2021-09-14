c2c729092a19aefb2a0e086af88308bd2b1d01d5
Milosz08 <gilgamilosz451@gmail.com>
2021-09-14 15:08:26 +0200

    create Header component with styles (styledComponent)

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,41 @@
+/**
+ * @file Header.jsx
+ * @author Miłosz Gilga (gilgamilosz451@gmail.com | milogil757@student.polsl.pl)
+ * @brief JavaScript React Stateless functional component (simplify state with React Hooks).
+ *
+ * @project "react-colour-generator"
+ * @version "^0.1.0"
+ *
+ * @date 09/14/2021
+ */
+
+import * as React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+import { HeaderContainer, HeaderLink, HeaderTitle, LineSeparator } from './Header.styles';
+
+/**
+ * Component responsible for generating header (static content), with link to github account.
+ *
+ * @returns { JSX.Element }
+ */
+const Header = () => (
+    <HeaderContainer>
+        <HeaderTitle>Colour Generator</HeaderTitle>
+        <HeaderLink>
+            <a
+                href = 'https://github.com/Milosz08'
+                target = '_blank'
+                rel = 'noreferrer'
+                title = 'Go to my Github profile'
+            >
+                <FontAwesomeIcon icon = {faGithub}/>
+            </a>
+        </HeaderLink>
+        <LineSeparator/>
+    </HeaderContainer>
+);
+
+export default Header;
---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,42 @@
+/**
+ * @file Header.styles.js
+ * @author Miłosz Gilga (gilgamilosz451@gmail.com | milogil757@student.polsl.pl)
+ * @brief JavaScript React Styled Components file.
+ *
+ * @project "react-colour-generator"
+ * @version "^0.1.0"
+ *
+ * @date 09/14/2021
+ */
+
+import styled from 'styled-components';
+import THEME from '../../utils/styles';
+
+export const HeaderContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 1.5rem;
+`;
+
+export const HeaderTitle = styled.div`
+    text-transform: uppercase;
+    color: ${THEME.TEXT_COLOUR};
+`;
+
+export const HeaderLink = styled.div`
+    font-size: 1rem;
+    a {
+        font-size: 1.5rem;
+        color: ${THEME.TEXT_COLOUR};
+    }
+`;
+
+export const LineSeparator = styled.aside`
+    width: 100%;
+    height: 1px;
+    background-color: ${THEME.MAGENTA_COLOUR};
+    margin-top: 15px;
+`;
