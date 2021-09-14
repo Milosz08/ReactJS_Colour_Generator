f649b5621da8062296db77d7d1c3bea5ca11a8e5
Milosz08 <gilgamilosz451@gmail.com>
2021-09-14 18:44:02 +0200

    create CenterColour component (with React StyledComponents styles)

---
+++ src/components/CenterColour/CenterColour.jsx
@@ -0,0 +1,46 @@
+/**
+ * @file BottomButtons.jsx
+ * @author Miłosz Gilga (gilgamilosz451@gmail.com | milogil757@student.polsl.pl)
+ * @brief JavaScript React Stateless functional component (simplify state with React Hooks).
+ *
+ * @project "react-colour-generator"
+ * @version "^0.1.0"
+ *
+ * @date 09/14/2021
+ */
+
+import React, { useContext } from 'react';
+import { StoreContext } from '../../store/StoreProvider';
+
+import { CenterColourContainer, HexInfo, RgbInfo } from './CenterColour.styles';
+
+/**
+ * Component responsible for generating the main colour panel along with rgb and hex value information.
+ *
+ * @returns { JSX.Element }
+ */
+const CenterColour = () => {
+
+    const { colourString, textColour, rgbRef, hexRef } = useContext(StoreContext);
+
+    return (
+        <CenterColourContainer
+            background = {colourString.hexC}
+        >
+            <RgbInfo readOnly
+                value = {colourString.rgbC}
+                colour = {textColour}
+                ref = {rgbRef}
+            >
+            </RgbInfo>
+            <HexInfo readOnly
+                value = {colourString.hexC.toLocaleUpperCase()}
+                colour = {textColour}
+                ref = {hexRef}
+            >
+            </HexInfo>
+        </CenterColourContainer>
+    );
+};
+
+export default CenterColour;
---
+++ src/components/CenterColour/CenterColour.styles.js
@@ -0,0 +1,42 @@
+/**
+ * @file CenterColour.styles.js
+ * @author Miłosz Gilga (gilgamilosz451@gmail.com | milogil757@student.polsl.pl)
+ * @brief JavaScript React Styled Components file.
+ *
+ * @project "react-colour-generator"
+ * @version "^0.1.0"
+ *
+ * @date 09/14/2021
+ */
+
+import styled from 'styled-components';
+import THEME from '../../utils/styles';
+const { BORDER_RADIUS, BUTTONS_BOX_SHADOW } = THEME;
+
+export const CenterColourContainer = styled.div.attrs(props => ({
+    style: { backgroundColor: props.background }
+}))`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 70%;
+    height: 200px;
+    margin-bottom: 50px;
+    border-radius: ${`${Number(BORDER_RADIUS.substring(0, 1)) * 1.5}px`};
+    box-shadow: ${BUTTONS_BOX_SHADOW};
+`;
+
+export const RgbInfo = styled.input`
+    text-align: center;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    font-size: 2.5rem;
+    color: ${props => props.colour};
+`;
+
+export const HexInfo = styled(RgbInfo)`
+    font-size: 2rem;
+    color: ${props => props.colour};
+`;
