31718f12d2d90ecd810d3bf58eb7b3c13765f6d7
Milosz08 <gilgamilosz451@gmail.com>
2021-09-14 15:11:35 +0200

    create global static variables styles for React StyledComponent

---
+++ src/utils/styles.js
@@ -0,0 +1,36 @@
+/**
+ * @file styles.js
+ * @author Miłosz Gilga (gilgamilosz451@gmail.com | milogil757@student.polsl.pl)
+ * @brief JavaScript React Styled Components file.
+ *
+ * @project "react-colour-generator"
+ * @version "^0.1.0"
+ *
+ * @date 09/14/2021
+ */
+
+/**
+ * Constants defined for React Styled Component.
+ * @type { object }
+ */
+const THEME = {
+
+    //Background gradient configuration
+    BACKGROUND_GRADIENT: `linear-gradient(45deg, #00C4C4, #632EB8)`,
+
+    //Colors
+    WHITE_COLOUR: '#ffffff',
+    LIGHTGRAY_COLOUR: '#d0d0d0',
+    TEXT_COLOUR: '#535353',
+    MAGENTA_COLOUR: '#5e72e4',
+    RED_COLOUR: '#7c2121',
+    GREEN_COLOUR: '#15581b',
+    BLUE_COLOUR: '#0c34b6',
+
+    //Misc
+    BORDER_RADIUS: '5px',
+    BOX_SHADOW: '2px 2px 5px 0 rgba(0, 0, 0, .2)',
+
+};
+
+export default THEME;
